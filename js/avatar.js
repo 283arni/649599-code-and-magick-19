29e2a850598c2e74f090a145c150277c32481b32
Aleksandr Arnautov <283arni@gmail.com>
2020-02-20 23:05:29 +0300

    module8-task1

---
+++ js/avatar.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function () {
+  var FILE_FORMATS = ['gif', 'png', 'jpg', 'jpeg'];
+
+  var fileBox = document.querySelector('input[type=file]');
+  var avatar = document.querySelector('.setup-user-pic');
+
+  fileBox.addEventListener('change', function () {
+    var file = fileBox.files[0];
+    var fileName = file.name.toLowerCase();
+
+    if (file) {
+      var matches = FILE_FORMATS.some(function (item) {
+        return fileName.endsWith(item);
+      });
+    }
+
+    if (matches) {
+      var readerImage = new FileReader();
+
+      readerImage.addEventListener('load', function () {
+        avatar.src = readerImage.result;
+      });
+
+      readerImage.readAsDataURL(file);
+    }
+  });
+})();
